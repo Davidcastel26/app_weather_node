cf58ce370bc86dadd8062e0317b33ff688601e66
David Castellanos <davcastellanoslarios@gmail.com>
2021-11-25 16:40:52 -0600

    adding the menu

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -9,26 +9,14 @@ const questions = [
         message:'what would you like to do?',
         choices:[
             {
-                value:'1',
-                name:`${'1.'.green} Create tasks`
+                value: 1,
+                name:`${'1.'.green} Search city`
             },{
-                value:'2',
-                name:`${'2.'.green}List tasks`
+                value: 2,
+                name:`${'2.'.green} History`
             },{
-                value:'3',
-                name:`${'3.'.green}List achived tasks`
-            },{
-                value:'4',
-                name:`${'4.'.green}List pending's tasks`
-            },{
-                value:'5',
-                name:`${'5.'.green}Complete tasks`
-            },{
-                value:'6',
-                name:`${'6.'.green}Delete tasks`
-            },{
-                value:"0",
-                name:`${'0.'.green}Exit`
+                value: 0,
+                name:`${'0.'.green} Exit`
             }
         ]
     }
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const { readInput, inquirerMenu, pause } = require("./helpers/inquirer")
+
+const main = async() => {
+    
+    // const text = await readInput('Hello: ');
+
+    let opt;
+
+    do{
+        // get the menu
+        opt = await inquirerMenu(); 
+        console.log({opt});
+        // pause the ejecution
+        await pause();
+
+    }while( opt !== 0)
+
+
+}
+
+main()
