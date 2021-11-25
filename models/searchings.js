f1b9abf1ce557d4836d2d63b70a1b3ab6ff354e9
David Castellanos <davcastellanoslarios@gmail.com>
2021-11-25 17:43:49 -0600

    adding the class without constructor

---
+++ models/searchings.js
@@ -0,0 +1,20 @@
+ 
+ class Searchings{
+
+    history = ['New York','Madrid','Medellin']
+
+    constructor(){
+        // TODO: read db if exist
+
+    }
+
+    async city(place = ''){
+        // petition http
+        console.log(place);
+
+        return [];// return all the places that i request
+    } 
+
+ }
+
+ module.exports = Searchings;
